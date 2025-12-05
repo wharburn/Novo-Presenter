4d4da0556cc0066f7870e4bd6ffaf86c5c5581be
Wayne Harburn <wayne@wharburn.com>
2025-12-05 06:45:37 +0000

    Fix Render build: exclude @humeai/voice-react from SSR webpack analysis

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
   },
   skipTrailingSlashRedirect: true,
   skipMiddlewareUrlNormalize: true,
+  // Exclude hume packages from being analyzed during SSR
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || []
+      config.externals.push('@humeai/voice-react')
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig

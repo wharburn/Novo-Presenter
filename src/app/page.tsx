603aa6a573167ff5fde7e20423d6c00206046143
Wayne Harburn <wayne@wharburn.com>
2025-12-05 13:57:44 +0000

    Immediately silence avatar when 10-second skip is triggered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
   const [skipToEnd, setSkipToEnd] = useState(false)
 
   const handleSkipToEnd = () => {
+    setIsSpeaking(false)  // Immediately silence avatar
     setHasStarted(true)
     setSkipToEnd(true)
   }

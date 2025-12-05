7c8a778357e41f7a27d5b4c2b12ef72e2a4ae7ae
Wayne Harburn <wayne@wharburn.com>
2025-12-05 01:43:06 +0000

    fix: Remove all padding above and below logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <header className="text-center flex-shrink-0 py-2">
+    <header className="text-center flex-shrink-0">
       <Image 
         src="/NovoPresent.png" 
         alt="NoVo Present" 
